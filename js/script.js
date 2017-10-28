4321a3ae26dc8d1186f08f581dac79c00120c191
Blake Wang <bwang@intouchhealth.com>
2017-10-28 15:42:10 +0000

    Reset clickTime on refresh

--- js/script.js
+++ js/script.js
@@ -1,13 +1,19 @@
 // Execute this script once jQuery has loaded.
 $(function() {
+  const clickFrequency = 666;    // Set click pitch;
+  const clickDuration = 0.05;    // Set click duration;
+
   var tempo = 60;                // Initialize tempo.
   var sPeriod = 60 / tempo;      // Calculate period in seconds.
   var msPeriod = 1000 * sPeriod; // Calculate period in miliseconds.
   var clickIndex = 0;            // Define current place in queue.
   var playing = false;           // Define variable to toggle when playing.
   var audioCtx = new (window.AudioContext || window.webkitAudioContext)(); // Initialize an AudioContext object.
-  const clickFrequency = 666;    // Set click pitch;
-  const clickDuration = 0.05;    // Set click duration;
+
+  function refreshAudioCtx() {
+    audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+    clickIndex = 0;
+  }
 
   // Schedule a single click.
   function scheduleClick() {
@@ -24,6 +30,7 @@ $(function() {
   var interval = null;
   function play() {
     playing = true;
+    refreshAudioCtx();
     interval = setInterval(scheduleClick, msPeriod);
   }
 
